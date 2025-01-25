f0a000b3de5d6232ab63961bc296655d199f352f
W4ldschr31n <nicolasrich@hotmail.fr>
2025-01-25 10:42:00 +0100

    character moves from anywhere

--- Assets/Scripts/Pathway.cs
+++ Assets/Scripts/Pathway.cs
@@ -8,6 +8,7 @@ public class Pathway : MonoBehaviour
 {
     public Transform[] waypoints;
     public CharacterMovement characterMovement;
+    public int currentCharacterPosition;
 
     private void Awake()
     {
@@ -17,12 +18,47 @@ public class Pathway : MonoBehaviour
 
     void Start()
     {
-        characterMovement.SetCourse(waypoints);
+        MoveCharacter(0);        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            currentCharacterPosition = 0;
+            MoveCharacter(2);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentCharacterPosition = 2;
+            MoveCharacter(0);
+        }
+    }
+
+    public void MoveCharacter(int index)
+    {
+        if (index >= waypoints.Length || index < 0)
+        {
+            Debug.Log("Waypoint index is out of range");
+            return;
+        }
+
+        if (index == currentCharacterPosition)
+        {
+            return;
+        }
         
+        Transform[] course;
+        if (index > currentCharacterPosition)
+        {
+             course = waypoints[currentCharacterPosition..(index+1)];
+        }
+        else
+        {
+            course = waypoints[index..(currentCharacterPosition+1)].Reverse().ToArray();
+        }
+        
+        characterMovement.SetCourse(course);
     }
 }
