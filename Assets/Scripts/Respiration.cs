298a7f2b4875961470c69f7fe7fd5e76bf24bd57
animalnevada <zte.a.juju@gmail.com>
2025-01-28 01:39:04 +0100

    Oxygen bar + light acard
    il est beaucoup trop tard  😢

--- Assets/Scripts/Respiration.cs
+++ Assets/Scripts/Respiration.cs
@@ -10,6 +10,7 @@ public class Respiration : MonoBehaviour
     public GameObject jauge; 
     [Header("ProgressBar")]
     public Image progressBar;
+    public Image bubble;
     public float maxRespiration = 100.0f;
     public float decayRate = 1.0f;
     public bool IsInWather;
@@ -43,6 +44,7 @@ public class Respiration : MonoBehaviour
         {   
             currentRespiration -= Time.deltaTime * decayRate;
             progressBar.fillAmount = currentRespiration / maxRespiration;
+            bubble.transform.localPosition = Vector3.Lerp(new Vector3(-460, -24, 0), new Vector3(390, -24, 0), currentRespiration / maxRespiration);
         }
         if (currentRespiration <= 0)
         {
